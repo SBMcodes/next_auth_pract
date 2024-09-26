795fdef9ccbb8ddd624abaecf2f83a2ec62fc321
SBMcodes <shreyankumar212@gmail.com>
2024-09-27 04:18:05 +0530

    Route setup in middleware

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,42 @@
 import authConfig from "./auth.config";
 import NextAuth from "next-auth";
 
+import {
+  DEFAULT_LOGIN_REDIRECT,
+  apiAuthPrefix,
+  authRoutes,
+  publicRoutes,
+} from "@/routes";
+
 const { auth } = NextAuth(authConfig);
 
 export default auth(async function middleware(req) {
   const isLoggedIn = !!req.auth;
-  console.log(isLoggedIn);
-  console.log("Route:", req.nextUrl.pathname);
+  const { nextUrl } = req;
+  const pathname = nextUrl.pathname;
+
+  const isApiAuthRoute = pathname.startsWith(apiAuthPrefix);
+  const isPublicRoute = publicRoutes.includes(pathname);
+  const isAuthRoute = authRoutes.includes(pathname);
+
+  if (isApiAuthRoute) {
+    return;
+  }
+
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+    }
+    return;
+  }
+
+  if (isPublicRoute) {
+    return;
+  }
+
+  if (!isLoggedIn) {
+    return Response.redirect(new URL("/auth/login", nextUrl));
+  }
 });
 
 // Matcher Stolen from:
