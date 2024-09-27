dc2125f1a6531bb874589b50ef98c7180363bf2b
SBMcodes <shreyankumar212@gmail.com>
2024-09-27 17:57:30 +0530

    oAuth setup

--- auth.config.ts
+++ auth.config.ts
@@ -4,8 +4,19 @@ import { LoginSchema } from "./schemas";
 import { getUserByEmail } from "./data/user";
 import bcrypt from "bcryptjs";
 
+import Github from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
+
 export default {
   providers: [
+    Github({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
     Credentials({
       async authorize(credentials) {
         console.log("Auth.config.ts");
--- components/auth/Social.tsx
+++ components/auth/Social.tsx
@@ -3,15 +3,30 @@ import React from "react";
 import { Button } from "../ui/button";
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
+// import { signIn } from "@/auth";
+
+// If we are trying to use signIn from client component
+// otherwise use previous statement for server actions
+import { signIn } from "next-auth/react";
+
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 
 const Social = () => {
+  const onClick = (provider: "google" | "github") => {
+    signIn(provider, {
+      callbackUrl: DEFAULT_LOGIN_REDIRECT,
+    });
+  };
+
   return (
     <div className="items-center flex w-full gap-x-2">
       <Button
         size="lg"
         className="w-full"
         variant={"outline"}
-        onClick={() => {}}
+        onClick={() => {
+          onClick("google");
+        }}
       >
         <FcGoogle className="h-5 w-5" />
       </Button>
@@ -19,7 +34,9 @@ const Social = () => {
         size="lg"
         className="w-full"
         variant={"outline"}
-        onClick={() => {}}
+        onClick={() => {
+          onClick("github");
+        }}
       >
         <FaGithub className="h-5 w-5" />
       </Button>
